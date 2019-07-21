df6a77ae55d1147c8fcbe27fb37669192e4d61c0
Ruba Sbeih <rsasbeih@gmail.com>
2019-07-21 23:27:48 +0300

    fixed ratio of menu and alert

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,14 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'FinalAlertGenerator';
+  icons=["../../assets/birthday-cake.svg",'../../assets/warning.svg','../../assets/pedestrian-walking.svg','../../assets/people.svg'];
+  title:string;
+  alertTitle:string;
+  color1="red";
+  color2="blue";
+  body:string;
+  iconSrc:string="../../assets/birthday-cake.svg";
+  setIcon(icon:string){
+    this.iconSrc=icon;
+  }
 }
